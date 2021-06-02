dbd9129d0d9b699475189c3d772aee9104504431
Veronica <veronicasmigielski@gmail.com>
2021-06-02 11:45:14 +0200

    Updated the css and data to reflect friends of mine and myself

--- src/data.js
+++ src/data.js
@@ -1,37 +1,39 @@
-export default [
+const data = [
     {
       id: 1,
-      name: 'Bertie Yates',
-      age: 29,
+      name: 'Veronica Smigielski',
+      age: 25,
       image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+        'https://s3.amazonaws.com/shecodesio-production/students/avatars/000/021/689/medium/Profile_picture.png?1612225148',
     },
     {
       id: 2,
-      name: 'Hester Hogan',
-      age: 32,
+      name: 'Giovanni Peeters',
+      age: 26,
       image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.6435-9/46702083_10216386509831659_4635785341918248960_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3k4BXWrEcGEAX9PB8U8&_nc_ht=scontent-bru2-1.xx&oh=581218b5d4ae1c893cad121a671e1747&oe=60DDF0EC',
     },
     {
       id: 3,
-      name: 'Larry Little',
-      age: 36,
+      name: 'Corwin Heirman',
+      age: 25,
       image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.18169-9/11204418_10200667842213699_2617894208255755735_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=R7VV9lFaY4UAX-usymj&_nc_ht=scontent-bru2-1.xx&oh=b778ec7c0ba77841b16a3cd5209cf796&oe=60DE547E',
     },
     {
       id: 4,
-      name: 'Sean Walsh',
-      age: 34,
+      name: 'Alyssa Irwin',
+      age: 24,
       image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.6435-9/131968510_3868804716503941_7721490282762195209_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=yuDlbvX8HFAAX8MXidQ&_nc_oc=AQlRpsc6uSs5DcgLKWoXr_vyarBDl7jJbhkToEL0_NYBFXNDUwYXoCZDs4X3bzSC_ayplAs_Pfwm5wWVlbSXu3I4&tn=E2Uh6PBpBYptjxbe&_nc_ht=scontent-bru2-1.xx&oh=de1f31e87530cdc96cebe9b084e32a8f&oe=60DC056C',
     },
     {
       id: 5,
-      name: 'Lola Gardner',
-      age: 29,
+      name: 'Hana Furst',
+      age: 61,
       image:
-        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        'https://scontent-bru2-1.xx.fbcdn.net/v/t31.18172-8/192179_209828162362181_2611622_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=0ZaAy1YC1RoAX-twuPf&tn=E2Uh6PBpBYptjxbe&_nc_ht=scontent-bru2-1.xx&oh=ea6329b9e52e1097e0798a95bf13d20c&oe=60DB76B5',
     },
   ];
+
+  export default data;
