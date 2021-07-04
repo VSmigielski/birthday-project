068cc78e3d14317ef9bb7e0f3183aad6ac8ad1e3
Veronica <veronicasmigielski@gmail.com>
2021-07-04 22:49:21 +0200

    Updated the picture links

--- src/data.js
+++ src/data.js
@@ -11,28 +11,28 @@ const data = [
       name: 'Giovanni Peeters',
       age: 26,
       image:
-        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.6435-9/46702083_10216386509831659_4635785341918248960_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3k4BXWrEcGEAX9PB8U8&_nc_ht=scontent-bru2-1.xx&oh=581218b5d4ae1c893cad121a671e1747&oe=60DDF0EC',
+        'images/Gio.jpg',
     },
     {
       id: 3,
       name: 'Corwin Heirman',
       age: 25,
       image:
-        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.18169-9/11204418_10200667842213699_2617894208255755735_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=R7VV9lFaY4UAX-usymj&_nc_ht=scontent-bru2-1.xx&oh=b778ec7c0ba77841b16a3cd5209cf796&oe=60DE547E',
+        'images/Corwin.jpg',
     },
     {
       id: 4,
       name: 'Alyssa Irwin',
       age: 24,
       image:
-        'https://scontent-bru2-1.xx.fbcdn.net/v/t1.6435-9/131968510_3868804716503941_7721490282762195209_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=yuDlbvX8HFAAX8MXidQ&_nc_oc=AQlRpsc6uSs5DcgLKWoXr_vyarBDl7jJbhkToEL0_NYBFXNDUwYXoCZDs4X3bzSC_ayplAs_Pfwm5wWVlbSXu3I4&tn=E2Uh6PBpBYptjxbe&_nc_ht=scontent-bru2-1.xx&oh=de1f31e87530cdc96cebe9b084e32a8f&oe=60DC056C',
+        'images/Alyssa.jpg',
     },
     {
       id: 5,
       name: 'Hana Furst',
       age: 61,
       image:
-        'https://scontent-bru2-1.xx.fbcdn.net/v/t31.18172-8/192179_209828162362181_2611622_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=0ZaAy1YC1RoAX-twuPf&tn=E2Uh6PBpBYptjxbe&_nc_ht=scontent-bru2-1.xx&oh=ea6329b9e52e1097e0798a95bf13d20c&oe=60DB76B5',
+        'images/Hana.jpg',
     },
   ];
 
